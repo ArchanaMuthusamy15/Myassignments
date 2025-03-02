4941aa700cbfed555b31a5b670140eba78bd98f1
ArchanaMuthusamy15 <archana.muthusamy15@gmail.com>
2025-03-02 17:54:42 +1100

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float Version =91.0f;
+		String developer="Google";
+		
+		boolean isaBeta=false;
+		int releaseYear=2008;
+		char browserlogo='C';
+				System.out.println("developer is"+" "+developer+" "+"Version is"+" "+Version);
+				System.out.println("releaseYear is"+" "+releaseYear+" "+"browserlogo is"+" "+browserlogo);
+		
+		
+		
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserlogo='f';
+		System.out.println("browserName is"+" "+browserName+" "+"browserVersion is"+" "+browserVersion+" "+"browserlogo is"+" "+browserlogo);
+		System.out.println("releaseYear is"+" "+releaseYear);
+		
+	}
+
+}
